fc54c0a4d13831e660516037ab306daa6e34f019
ThorinII <flipsidelachy@gmail.com>
2014-03-13 11:45:23 +1030

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "Time Tracker II"
+
+version := "1.0"
+
+scalaVersion := "2.10.3"
+
---
+++ src/main/scala/me/lachlanap/timetrackerii/Main.scala
@@ -0,0 +1,7 @@
+package me.lachlanap.timetrackerii
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+}
